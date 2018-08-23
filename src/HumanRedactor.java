e6f20c16e8d13203c77a1e8abbefc73d75420eeb
AngyUg <proofmyself@gmail.com>
2018-08-23 09:59:20 +0300

    n problem

--- src/HumanRedactor.java
+++ src/HumanRedactor.java
@@ -1,9 +1,9 @@
-public class HumanRedactor  {
+public class HumanRedactor {
 
 
-    public Human[] addHuman (Human[] arr, Human human){
-        for (int i = 0; i <arr.length; i++) {
-            if (arr[i]==null){
+    public Human[] addHuman(Human[] arr, Human human) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
                 arr[i] = human;
                 return arr;
             }
@@ -11,21 +11,23 @@ public class HumanRedactor  {
         return arr;
     }
 
-    
-    public Human[] readHuman (Human[]arr,Human human){
+
+    public Human[] readHuman(Human[] arr, Human human) {
         return arr;
     }
 
-    public Human[] updateHuman (Human[] arr, Human human,int index){
-        for (int i = 0; i <arr.length; i++){
-            arr[index]=human;
+    public Human[] updateHuman(Human[] arr, Human human, int index) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[index] = human;
             return arr;
         }
+
         return arr;
     }
-    public Human[] deleteHuman (Human[] arr, Human human,int index){
-        for (int i = 0; i <arr.length; i++){
-            arr[index]=null;
+
+    public Human[] deleteHuman(Human[] arr, Human human, int index) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[index] = null;
             return arr;
         }
         return arr;
