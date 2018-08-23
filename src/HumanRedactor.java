72f3607934ba31c7aad538f4b88255d67d272da7
AngyUg <proofmyself@gmail.com>
2018-08-23 09:48:22 +0300

    readHuman upHuman delHuman

---
+++ src/HumanRedactor.java
@@ -0,0 +1,34 @@
+public class HumanRedactor  {
+
+
+    public Human[] addHuman (Human[] arr, Human human){
+        for (int i = 0; i <arr.length; i++) {
+            if (arr[i]==null){
+                arr[i] = human;
+                return arr;
+            }
+        }
+        return arr;
+    }
+
+    public Human[] readHuman (Human[]arr,Human human){
+        return arr;
+    }
+
+    public Human[] updateHuman (Human[] arr, Human human,int index){
+        for (int i = 0; i <arr.length; i++){
+            arr[index]=human;
+            return arr;
+        }
+        return arr;
+    }
+    public Human[] deleteHuman (Human[] arr, Human human,int index){
+        for (int i = 0; i <arr.length; i++){
+            arr[index]=null;
+            return arr;
+        }
+        return arr;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] arr = new String[10];
+        Menu menu = new Menu();
+
+        //Human human = new Human();
+        //Human human1 = new Human(10);
+        Human human2 = new Human(1, "name", "lastName", 30);
+
+       // System.out.println(human.toString());
+        //System.out.println(human1.toString());
+        //System.out.println(human2.toString());
+
+
+//        while (!menu.theEndProgram){
+//            menu.mainMenu();
+//            arr = menu.route(arr);
+//        }
+
+    HumanRedactor humanRedactor=new HumanRedactor();
+    }
+
+}
